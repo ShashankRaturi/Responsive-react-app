e55e4b698956707cb2c6a7e4fb129632c48a6f19
Shashank <shashankraturi4@gmail.com>
2023-03-09 11:14:42 +0530

    project finished , now the fun begins

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router  , Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,7 +14,11 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' exact element={<Home/>} />   {/*this is the updated syntax */}
+        <Route path='/services' exact element={<Services/>}></Route>
+        <Route path='/products' exact element={<Products/>}></Route>
+        <Route path='/sign-up' exact element={<SignUp/>}></Route>
       </Routes>
+      <Footer/>
     </Router>
       
     </>
